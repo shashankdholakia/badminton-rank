0819bea609948ff462de4912b6ad6843d7b03ec7
Shishir Dholakia <dholakia.shishir@berkeley.edu>
2019-01-11 15:49:05 -0800

    Finished first working version of scraper.py

--- scraper.py
+++ scraper.py
@@ -23,20 +23,54 @@ def nth_repl(s, sub, repl, nth):
     if i == nth:
         return s[:find]+repl+s[find + len(sub):]
     return s
+def winnerlister(list_of_matches):
 
-
+    for i,line in enumerate(list_of_matches):
+        if "draw=" in line:
+            if 'match-info' in list_of_matches[i+3]:
+                singleswinnerlist.append(list_of_matches[i+1])
+                singlesloserlist.append(list_of_matches[i+2])
+            if 'match-info' in list_of_matches[i+5]:
+                doubleswinnerlist.append(list_of_matches[i+1])
+                doubleswinnerlist.append(list_of_matches[i+2])
+                doublesloserlist.append(list_of_matches[i+3])
+                doublesloserlist.append(list_of_matches[i+4])
+    return[singleswinnerlist,singlesloserlist,doubleswinnerlist,doublesloserlist]
+    
+    
+string = 'http://tournamentsoftware.com'
 lines = [line.rstrip('\n') for line in open('tournaments.txt')]
 #for line in lines:
 line = lines[0]
 print(line)
+print('blah')
 s = requests.Session() 
 s.post(line, cookies = {'st': r'l=1033&exp=43841.9228685648&c=1'})
-r = s.get(line)
-
+matcheslistlink = nth_repl(line,'tournament','matches',2)
+r = s.get(matcheslistlink)
 html_page = r.content
 soup = BeautifulSoup(html_page, "lxml")
+list_of_matches = []
+dayslist = []
+singleswinnerlist = []
+doubleswinnerlist = []
+doublesloserlist = []
+singlesloserlist = []
 for link in soup.find_all('a'):
-    print(link.get('href')) 
+    if '&d=' in link.get('href'):
+        dayslist.append(link.get('href'))
+for day in dayslist:
+    r = s.get(string+day)
+    html_page_matchesofthatday = r.content
+    soup = BeautifulSoup(html_page_matchesofthatday, "lxml")
+    list_of_matches = [line.get('href') for line in soup.find_all('a')]
+    print(len(list_of_matches))
+    singleswinnerlist,singlesloserlist,doubleswinnerlist,doublesloserlist = winnerlister(list_of_matches)
+        
+#html_page_draws = r.content
+#soup = BeautifulSoup(html_page_draws, "lxml")
+#for namelink in soup.find_all('a'):
+#    print(namelink.get('href'))
 #drawslink = nth_repl(tournamentlink,'tournament','draws',2)
 #html_page_draws = urllib2.urlopen(drawslink)
 #soup = BeautifulSoup(html_page_draws, "lxml")
